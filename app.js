b3c7ce3721d44f0344c7f422acce84461dbc82c9
wangaw@94gmail.com <wangaw94@gmail.com>
2022-06-16 15:59:20 +0300

    Clearing the input fields after every entry

--- app.js
+++ app.js
@@ -110,6 +110,20 @@ var UIController = (function(){
 
     },
 
+    clearFields: function(){
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+        
+      });
+
+    },
+
     getDOMstrings: function(){
       return DOMstrings;
     }
@@ -117,11 +131,9 @@ var UIController = (function(){
 })();
 
 
-
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-  
 
   var setupEventListeners = function(){
 
@@ -138,16 +150,13 @@ var controller = (function(budgetCtrl, UICtrl){
     });
 
   }
-
   
-
   var ctrlAddItem = function(){
     var input, newItem;
 
     // 1. Get the field input data.
     var input = UICtrl.getInput();
     
-
     //2. Add the item to the budget controller
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
@@ -155,9 +164,12 @@ var controller = (function(budgetCtrl, UICtrl){
 
     UICtrl.addListItem(newItem, input.type)
 
-    //4. Calculate the budget
+    //4. Clear the fields after every entry
+    UICtrl.clearFields()
 
-    //5. Display the budget on the UI
+    //5. Calculate the budget
+
+    //6. Display the budget on the UI
 
 
   };
