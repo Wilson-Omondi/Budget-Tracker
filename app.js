b4fa4e6820569ef302166a531e7b3bb5324fde9d
wangaw@94gmail.com <wangaw94@gmail.com>
2022-06-20 13:18:28 +0300

    Updating the Budget Controller

--- app.js
+++ app.js
@@ -13,9 +13,17 @@ var budgetController = (function(){
     this.value = value;
   };
 
-  var allExpense = [];
-  var allIncome = [];
-  var totalExpenses = 0;
+  // var allExpense = [];
+  // var allIncome = [];
+  // var totalExpenses = 0;
+
+  var calculateTotal = function(type){
+    var sum = 0;
+    data.allItems[type].forEach(function(curr){
+      sum = sum + curr.value;
+    });
+    data.totals[type] = sum;
+  }
 
   var data = {
 
@@ -27,7 +35,9 @@ var budgetController = (function(){
     totals: {
       exp: 0,
       inc: 0
-    } 
+    },
+    budget: 0,
+    percentage: -1 
   };
 
   return {
@@ -55,6 +65,34 @@ var budgetController = (function(){
 
       //return the new element
       return newItem;
+    },
+
+    calculateBudget: function() {
+
+      // calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      //calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+
+      //calculate the percentage of income that we spent
+      if(data.totals.inc > 0){
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100)
+      } else {
+        data.percentage = -1;
+      }
+      
+    },
+
+    getBudget: function(){
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
+
     },
 
   };
@@ -152,9 +190,12 @@ var controller = (function(budgetCtrl, UICtrl){
   };
 
   var updateBudget = function(){
+
     //1. Calculate the budget
+    budgetCtrl.calculateBudget();
 
     //2. Return the budget
+    var budget = budgetCtrl.getBudget();
 
     //3. Display the budget on the UI
   }
